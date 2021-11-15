c67be2e2717448c34957095d80edac28fa4a870e
Ed <lookatemail@gmail.com>
2021-11-15 23:18:58 +0000

    Refactor navBar component

--- packages/bad-theme/src/components/header/navigationActions.js
+++ packages/bad-theme/src/components/header/navigationActions.js
@@ -6,14 +6,75 @@ import { Dropdown, DropdownButton, NavDropdown } from "react-bootstrap";
 import { colors } from "../../config/colors";
 
 const NavigationActions = ({ state, actions }) => {
-  const [wpMenu, setWpMenu] = useState([]);
+  const [wpMainMenu, setWpMainMenu] = useState([]);
+  const [wpMoreMenu, setWpMoreMenu] = useState([]);
+  const NAV_DIVIDER = 8;
 
   useEffect(() => {
     const data = state.source.data[`/menu/primary-menu/`].items;
-    setWpMenu(data);
+    const dataLength = data.length;
+
+    setWpMainMenu(data.slice(0, NAV_DIVIDER));
+    setWpMoreMenu(data.slice(NAV_DIVIDER, dataLength));
   }, []);
 
   // SERVERS ---------------------------------------------
+  const ServeNestedMenu = ({ item }) => {
+    const { ID, title, slug, child_items } = item;
+
+    return (
+      <div>
+        <Dropdown.Item key={ID} href={`${slug}`}>
+          {title}
+        </Dropdown.Item>
+        {child_items.map((item) => {
+          const { ID, title, slug } = item;
+
+          return (
+            <div
+              key={ID}
+              style={{
+                paddingLeft: 20,
+                backgroundColor: colors.lightSilver,
+              }}
+            >
+              <Dropdown.Item href={`${slug}`}>{title}</Dropdown.Item>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  const ServeMoreMenu = () => {
+    if (!wpMoreMenu.length) return null;
+
+    return (
+      <div className="dropdown">
+        <Dropdown>
+          <Dropdown.Toggle variant="btn-m" style={styles.dropDownBtn}>
+            More
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu style={{ backgroundColor: colors.lightSilver }}>
+            {wpMoreMenu.map((item) => {
+              const { ID, title, slug } = item;
+
+              if (item.child_items)
+                return <ServeNestedMenu key={ID} item={item} />;
+
+              return (
+                <div key={ID}>
+                  <Dropdown.Item href={`${slug}`}>{title}</Dropdown.Item>
+                </div>
+              );
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    );
+  };
+
   const ServeDropDownMenu = ({ item }) => {
     const { title, slug, child_items } = item;
 
@@ -26,10 +87,10 @@ const NavigationActions = ({ state, actions }) => {
 
           <Dropdown.Menu style={{ backgroundColor: colors.lightSilver }}>
             {child_items.map((item) => {
-              const { title, slug } = item;
+              const { ID, title, slug } = item;
 
               return (
-                <div key={item.ID}>
+                <div key={ID}>
                   <Dropdown.Item href={`${slug}`}>{title}</Dropdown.Item>
                 </div>
               );
@@ -42,7 +103,6 @@ const NavigationActions = ({ state, actions }) => {
 
   const ServeMainNavigation = ({ item }) => {
     const { title, slug } = item;
-    console.log(item);
 
     if (item.child_items) return <ServeDropDownMenu item={item} />;
 
@@ -58,31 +118,12 @@ const NavigationActions = ({ state, actions }) => {
   return (
     <div>
       <div className="flex-row" style={styles.container}>
-        <Link link="/" style={styles.link}>
-          Guidance & Standards
-        </Link>
-        <Link link="/" style={styles.link}>
-          Clinical Services
-        </Link>
-        <Link link="/" style={styles.link}>
-          Education & Training
-        </Link>
-        <Link link="/" style={styles.link}>
-          Events
-        </Link>
-        <Link link="/" style={styles.link}>
-          Journal & Research
-        </Link>
-        <Link link="/" style={styles.link}>
-          Membership
-        </Link>
+        {wpMainMenu.map((item) => {
+          const { ID } = item;
 
-        {/* <ServeDropDownMenu /> */}
-      </div>
-      <div className="flex-row" style={styles.container}>
-        {wpMenu.map((item) => {
-          return <ServeMainNavigation key={item.ID} item={item} />;
+          return <ServeMainNavigation key={ID} item={item} />;
         })}
+        <ServeMoreMenu />
       </div>
     </div>
   );
