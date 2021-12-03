aedf5d6f677826ac6aa7cf2c77825102a0b0a0c0
Ed <lookatemail@gmail.com>
2021-12-03 09:28:53 +0000

    Refactor social icons - able to add multiple sets

--- packages/bad-theme/src/components/socialIcons.js
+++ packages/bad-theme/src/components/socialIcons.js
@@ -10,31 +10,77 @@ import Instagram from "../img/svg/instagramColour.svg";
 const SocialIcons = ({ state, actions, libraries, block }) => {
   const Html2React = libraries.html2react.Component; // Get the component exposed by html2react.
 
+  if (!block.social_links) return null;
+
   const marginHorizontal = state.theme.marginHorizontal;
   const marginVertical = state.theme.marginVertical;
+
+  const { links_per_row, social_links } = block;
+
+  let COLUMNS_NUMBER = `1fr`;
+  if (links_per_row === "Two") COLUMNS_NUMBER = ` 1fr 1fr`;
+  if (links_per_row === "Three") COLUMNS_NUMBER = ` 1fr 1fr 1fr`;
+
+  const ServeIcon = ({ url, icon }) => {
+    if (!url) return null;
+
+    return (
+      <div style={styles.socials}>
+        <Link link={url} target="_blank">
+          <Image
+            src={icon}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
+          />
+        </Link>
+      </div>
+    );
+  };
+
+  const ServeTitle = ({ title }) => {
+    if (!title) return null;
+
+    return (
+      <div
+        style={{
+          fontSize: 24,
+          fontWeight: "bold",
+          color: colors.black,
+          paddingBottom: `1em`,
+        }}
+      >
+        <Html2React html={title} />
+      </div>
+    );
+  };
 
   // RETURN ---------------------------------------------------
   return (
     <div style={{ margin: `${marginVertical}px ${marginHorizontal}px` }}>
       <div
-        className="flex"
-        style={{ justifyContent: "center", padding: `2em 0` }}
+        style={{
+          display: "grid",
+          gridTemplateColumns: COLUMNS_NUMBER,
+          gap: 20,
+        }}
       >
-        <div style={styles.socials}>
-          <Link link={`https://www.facebook.com/`} target="_blank">
-            <Image src={Facebook} className="d-block h-100" alt="Facebook" />
-          </Link>
-        </div>
-        <div style={styles.socials}>
-          <Link link={`https://www.twitter.com/`} target="_blank">
-            <Image src={Twitter} className="d-block h-100" alt="Twitter" />
-          </Link>
-        </div>
-        <div style={styles.socials}>
-          <Link link={`https://www.instagram.com/`} target="_blank">
-            <Image src={Instagram} className="d-block h-100" alt="Instagram" />
-          </Link>
-        </div>
+        {social_links.map((block, key) => {
+          return (
+            <div key={key}>
+              <div className="flex" style={{ justifyContent: "center" }}>
+                <ServeTitle title={block.title} />
+              </div>
+              <div className="flex" style={{ justifyContent: "center" }}>
+                <ServeIcon icon={Facebook} url={block.facebook} />
+                <ServeIcon icon={Twitter} url={block.twitter} />
+                <ServeIcon icon={Instagram} url={block.instagram} />
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
