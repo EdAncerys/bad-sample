5da6c2de58f5d1b781cdc99eae634b2df4438606
Ed <lookatemail@gmail.com>
2021-12-14 11:05:47 +0000

    Remove block title names

--- packages/bad-theme/src/components/builder/blockBuilder.js
+++ packages/bad-theme/src/components/builder/blockBuilder.js
@@ -39,6 +39,8 @@ const BlocksBuilder = ({ state, actions, libraries, blocks, isMenu }) => {
 
   // SERVERS -----------------------------------------------------
   const ServeBlockTitle = ({ acf_fc_layout }) => {
+    return null; // add block name title
+
     return (
       <div
         style={{
