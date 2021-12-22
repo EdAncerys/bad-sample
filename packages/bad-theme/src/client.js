98892fd96a955c1257ba3384957da2531f121517
Ed <lookatemail@gmail.com>
2021-12-22 13:45:34 +0000

    Change main Roboto font wight

--- packages/bad-theme/src/client.js
+++ packages/bad-theme/src/client.js
@@ -65,7 +65,7 @@ const BADTheme = {
 
           WebFontLoader.load({
             google: {
-              families: ["Roboto", "Lato"],
+              families: ["Roboto:700", "Lato"],
             },
           });
         });
