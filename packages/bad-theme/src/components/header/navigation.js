ec705b44e36e460cbc7c7484a5ea692c74fa6d4a
Ed <lookatemail@gmail.com>
2022-01-15 14:04:45 +0000

    Cleanup nav component & refactor

--- packages/bad-theme/src/components/header/navigation.js
+++ packages/bad-theme/src/components/header/navigation.js
@@ -57,23 +57,6 @@ const Navigation = ({ state, actions, libraries }) => {
     }
   };
 
-  const handleMenuHover = ({ onMouseEnter }) => {
-    const attractor = document.querySelector(
-      `#menu-shadow-${activeMenu.current}`
-    );
-
-    if (state.theme.activeMenuItem === null && activeMenu.current !== null)
-      activeMenu.current = null; // reset useRef hooks after dropdown dismiss event
-
-    if (activeMenu.current !== activeMenu.current) {
-      if (onMouseEnter) {
-        attractor.classList.remove("d-none");
-      } else {
-        attractor.classList.add("d-none");
-      }
-    }
-  };
-
   const handleActiveMenu = ({ slug, removeShadow }) => {
     const selector = document.querySelector(`#menu-${slug}`);
     const shadowColor = handleBoxShadow(slug);
@@ -83,302 +66,10 @@ const Navigation = ({ state, actions, libraries }) => {
       return;
     }
     if (activeMenu.current === slug) selector.style.boxShadow = shadowColor;
-
-    console.log(slug);
   };
 
   // SERVERS -----------------------------------------------------
-  const ServeMenuDropDown = ({ title, menu, url }) => {
-    if (!menu.length) return null;
-    const isMoreMenu = title === "More";
-
-    const ServeTitle = ({ title, url, child_items }) => {
-      if (!title || !url) return null;
-
-      const ServeMenuArrow = () => {
-        if (!child_items) return null;
-
-        return (
-          <KeyboardArrowRightIcon
-            style={{
-              fill: colors.darkSilver,
-              borderRadius: "50%",
-              padding: 0,
-            }}
-          />
-        );
-      };
-
-      return (
-        <NavDropdown.Item
-          className="flex pointer"
-          style={{
-            alignItems: "center",
-            padding: `1em 0 1em`,
-            borderBottom: `1px dotted ${colors.darkSilver}`,
-            whiteSpace: "normal",
-          }}
-          onClick={() => setGoToAction({ path: url, actions })}
-          onMouseEnter={(e) => {
-            if (!e.target.innerText) return null; // prevents passing empty title object
-
-            childMenuRef.current = e.target.innerText;
-            const title = {
-              url: url,
-              title: childMenuRef.current,
-            };
-            state.theme.childMenuRef = title;
-          }}
-        >
-          <div className="flex-row">
-            <div className="flex" style={{ textTransform: "capitalize" }}>
-              <Html2React html={title} />
-            </div>
-            <ServeMenuArrow />
-          </div>
-        </NavDropdown.Item>
-      );
-    };
-
-    return (
-      <NavDropdown
-        title={<Html2React html={title} /> || "Menu Title"}
-        style={{
-          display: "flex", // static position adding ability for dropdown to move up the scope
-          position: "static",
-          height: "100%",
-          alignItems: "center",
-        }}
-        onClick={() => {
-          if (isMoreMenu) {
-            const activeMenuItem = document.querySelector(
-              `#menu-shadow-${activeMenu.current}`
-            );
-            if (activeMenuItem) activeMenuItem.classList.add("d-none");
-            activeMenu.current = null;
-            state.theme.activeMenuItem = null;
-          }
-        }}
-      >
-        <div
-          className="flex"
-          style={{
-            padding: `2em 4em`,
-            height: BANNER_HEIGHT,
-            backgroundColor: colors.lightSilver, // nav bar dropdown background color
-            boxShadow: `0 0.5rem 1rem rgba(0, 0, 0, 0.15)`,
-          }}
-        >
-          <div
-            style={{
-              overflow: "auto",
-              width: 400,
-            }}
-          >
-            <ServeTitle title={title} url={url} />
-            {menu.map((item, key) => {
-              const { title, url, child_items } = item;
-
-              return (
-                <div
-                  key={key}
-                  className="flex-row"
-                  style={{ marginLeft: isMoreMenu ? 0 : `1em` }}
-                >
-                  <ServeTitle
-                    title={title}
-                    url={url}
-                    child_items={child_items}
-                  />
-                </div>
-              );
-            })}
-          </div>
-          {menu.map((menu, key) => {
-            return <ChildMenu key={key} menu={menu} />;
-          })}
-          <NavBarDropDownContent />
-        </div>
-      </NavDropdown>
-    );
-  };
-
-  const ServeMenu = () => {
-    return (
-      <div className="flex" style={styles.container}>
-        {wpMainMenu.map((item, key) => {
-          const { title, slug, url, child_items } = item;
-
-          if (child_items)
-            return (
-              <div
-                key={key}
-                className={"main-menu-container " + key}
-                style={{ height: "100%" }}
-                onMouseEnter={() => {
-                  activeMenu.current = key;
-                  handleMenuHover({ onMouseEnter: true });
-                }}
-                onMouseLeave={() => {
-                  // handleMenuHover({ remove });
-                  activeMenu.current = null; // clear Ref hook after handler been triggered only!
-                }}
-                // onClick={handleActiveMenu}
-              >
-                <ServeMenuDropDown
-                  style={{ height: "100%" }}
-                  title={title}
-                  url={url}
-                  menu={child_items}
-                />
-
-                <div
-                  id={`menu-shadow-${key}`}
-                  className="d-none"
-                  style={{
-                    backgroundColor: handleBoxShadow(item.slug),
-                    height: 5,
-                    width: "100%",
-                    position: "relative",
-                    bottom: 5,
-                  }}
-                />
-              </div>
-            );
-
-          return (
-            <div key={key}>
-              <Nav.Link
-                style={styles.link}
-                onClick={() => setGoToAction({ path: url, actions })}
-              >
-                <Html2React html={title} />
-              </Nav.Link>
-            </div>
-          );
-        })}
-
-        <ServeMenuDropDown title="More" menu={wpMoreMenu} />
-      </div>
-    );
-  };
-
-  const ServeMenuDropDownB = ({ title, menu, url }) => {
-    if (!menu.length) return null;
-    const isMoreMenu = title === "More";
-
-    const ServeTitle = ({ title, url, child_items }) => {
-      if (!title || !url) return null;
-
-      const ServeMenuArrow = () => {
-        if (!child_items) return null;
-
-        return (
-          <KeyboardArrowRightIcon
-            style={{
-              fill: colors.darkSilver,
-              borderRadius: "50%",
-              padding: 0,
-            }}
-          />
-        );
-      };
-
-      return (
-        <li
-          className="flex pointer"
-          style={{
-            alignItems: "center",
-            padding: `1em 0 1em`,
-            borderBottom: `1px dotted ${colors.darkSilver}`,
-            whiteSpace: "normal",
-          }}
-          onClick={() => setGoToAction({ path: url, actions })}
-          onMouseEnter={(e) => {
-            if (!e.target.innerText) return null; // prevents passing empty title object
-
-            childMenuRef.current = e.target.innerText;
-            const title = {
-              url: url,
-              title: childMenuRef.current,
-            };
-            state.theme.childMenuRef = title;
-          }}
-        >
-          <div className="flex-row">
-            <div className="flex" style={{ textTransform: "capitalize" }}>
-              <Html2React html={title} />
-            </div>
-            <ServeMenuArrow />
-          </div>
-        </li>
-      );
-    };
-
-    return (
-      <NavDropdown
-        title={<Html2React html={title} /> || "Menu Title"}
-        style={{
-          display: "flex", // static position adding ability for dropdown to move up the scope
-          position: "static",
-          height: "100%",
-          alignItems: "center",
-        }}
-        onClick={() => {
-          if (isMoreMenu) {
-            const activeMenuItem = document.querySelector(
-              `#menu-shadow-${activeMenu.current}`
-            );
-            if (activeMenuItem) activeMenuItem.classList.add("d-none");
-            activeMenu.current = null;
-            state.theme.activeMenuItem = null;
-          }
-        }}
-      >
-        <div
-          className="flex"
-          style={{
-            padding: `2em 4em`,
-            height: BANNER_HEIGHT,
-            backgroundColor: colors.lightSilver, // nav bar dropdown background color
-            boxShadow: `0 0.5rem 1rem rgba(0, 0, 0, 0.15)`,
-          }}
-        >
-          <div
-            style={{
-              overflow: "auto",
-              width: 400,
-            }}
-          >
-            <ServeTitle title={title} url={url} />
-            {menu.map((item, key) => {
-              const { title, url, child_items } = item;
-
-              return (
-                <div
-                  key={key}
-                  className="flex-row"
-                  style={{ marginLeft: isMoreMenu ? 0 : `1em` }}
-                >
-                  <ServeTitle
-                    title={title}
-                    url={url}
-                    child_items={child_items}
-                  />
-                </div>
-              );
-            })}
-          </div>
-          {menu.map((menu, key) => {
-            return <ChildMenu key={key} menu={menu} />;
-          })}
-          <NavBarDropDownContent />
-        </div>
-      </NavDropdown>
-    );
-  };
-
-  const ServeMenuB = () => {
+  const ServeMenu = ({ secondaryMenu }) => {
     const ServeMenuChildren = ({ item }) => {
       const { title, slug, url, child_items } = item;
 
@@ -431,7 +122,9 @@ const Navigation = ({ state, actions, libraries }) => {
                     onClick={() => setGoToAction({ path: url, actions })}
                   >
                     <div className="flex">
-                      <Html2React html={title} />
+                      <div className="menu-title">
+                        <Html2React html={title} />
+                      </div>
                     </div>
                     <ServeMenuArrow />
                   </a>
@@ -480,36 +173,13 @@ const Navigation = ({ state, actions, libraries }) => {
 
   return (
     <BlockWrapper>
-      <div className="row roboto">
-        <div className="col-md-12">
-          <Navbar collapseOnSelect expand="lg">
-            <Container>
-              <Navbar.Toggle
-                aria-controls="responsive-navbar-nav"
-                className="m-2"
-              />
-              <Navbar.Collapse>
-                <div
-                  className="flex"
-                  style={{ height: "4em", margin: `0 ${marginHorizontal}px` }}
-                >
-                  <Nav className="flex BAD-menu">
-                    <ServeMenu />
-                  </Nav>
-                </div>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-        </div>
-      </div>
-
       <nav
         className="navbar navbar-expand-lg roboto"
         style={{ margin: `0 ${marginHorizontal}px` }}
       >
         <div className="container-fluid">
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ServeMenuB />
+            <ServeMenu />
 
             <ul className="navbar-nav">
               <li className="nav-item dropdown">
