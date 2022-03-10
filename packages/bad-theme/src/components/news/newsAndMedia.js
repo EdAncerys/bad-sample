e236cfd0d197be7c432c2d6df9e12e433cee87c6
Ed <lookatemail@gmail.com>
2022-03-10 13:44:24 +0000

    Add snags to events block

--- packages/bad-theme/src/components/news/newsAndMedia.js
+++ packages/bad-theme/src/components/news/newsAndMedia.js
@@ -44,7 +44,7 @@ const NewsAndMedia = ({ state, actions, libraries, block }) => {
   const categoryFilterRef = useRef("");
   const dateFilterRef = useRef("");
   const yearFilterRef = useRef("");
-  const loadMoreRef = useRef("");
+  const loadMoreRef = useRef(true);
 
   const marginHorizontal = state.theme.marginHorizontal;
   let marginVertical = state.theme.marginVertical;
@@ -68,15 +68,17 @@ const NewsAndMedia = ({ state, actions, libraries, block }) => {
     if (data.length === 0) return;
 
     // filter by categories
-    if (category_filter && category_filter !== "0")
-      data = data.filter((item) =>
-        item.categories.includes(Number(category_filter))
-      );
+    // if (category_filter && category_filter !== "0")
+    //   data = data.filter((item) =>
+    //     item.categories.includes(Number(category_filter))
+    //   );
 
     // apply sort by date functionality
     data = data.sort((a, b) => new Date(b.date) - new Date(a.date));
-    // console.log("category_filter: ", category_filter); // debug
-    // console.log("data: ", data); // debug
+    if (post_limit) data = data.slice(0, Number(post_limit)); // apply limit on posts
+    console.log("category_filter: ", category_filter); // debug
+    console.log("post_limit: ", post_limit); // debug
+    console.log("data: ", data); // debug
 
     setFilterList(data);
     if (state.source.category) {
@@ -121,14 +123,35 @@ const NewsAndMedia = ({ state, actions, libraries, block }) => {
   };
 
   const handleLoadMoreFilter = () => {
-    const limit = post_limit || 6;
-    let postList = Object.values(state.source.post); // add filterList object to data array
+    let data = Object.values(state.source.post); // add filterList object to data array
+    // filter by categories
+    // if (category_filter && category_filter !== "0")
+    //   data = data.filter((item) =>
+    //     item.categories.includes(Number(category_filter))
+    //   );
     // apply sort by date functionality
-    postList = postList.sort((a, b) => new Date(b.date) - new Date(a.date));
-    if (loadMoreRef.current) postList = postList.slice(0, Number(limit)); // apply limit on posts
+    data = data.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-    setFilterList(postList);
-    loadMoreRef.current = !loadMoreRef.current;
+    // if (loadMoreRef.current) data = data.slice(0, Number(limit)); // apply limit on posts
+    // // filter by categories
+    // if (category_filter && category_filter !== "0")
+    //   data = data.filter((item) =>
+    //     item.categories.includes(Number(category_filter))
+    //   );
+
+    // if loadMore is truthy display all filterList posts
+    if (loadMoreRef.current) {
+      setFilterList(data);
+      loadMoreRef.current = false;
+    } else {
+      // if loadMore is falsy display only limit posts
+      if (post_limit) data = data.slice(0, Number(post_limit)); // apply limit on posts
+      setFilterList(data);
+      loadMoreRef.current = true;
+    }
+
+    // setFilterList(data);
+    // loadMoreRef.current = !loadMoreRef.current;
   };
 
   const handleSearch = () => {
@@ -419,9 +442,16 @@ const NewsAndMedia = ({ state, actions, libraries, block }) => {
   };
 
   const ServeMoreAction = () => {
-    if (isLayoutOne || filterList.length === 0) return null;
+    // if filterList is empty || lest than post_limit return null
+    if (
+      isLayoutOne ||
+      filterList.length === 0 ||
+      filterList.length < post_limit ||
+      !post_limit
+    )
+      return null;
 
-    const value = loadMoreRef.current ? "Less" : " Load More";
+    const value = !loadMoreRef.current ? "Less" : " Load More";
 
     return (
       <div
