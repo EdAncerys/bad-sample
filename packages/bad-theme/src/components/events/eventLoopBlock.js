a3394a6a1d65977cd1cdc3da62c997e74916f7f9
Ed <lookatemail@gmail.com>
2022-03-18 17:00:29 +0000

    Add snags to event & refactor filter object

--- packages/bad-theme/src/components/events/eventLoopBlock.js
+++ packages/bad-theme/src/components/events/eventLoopBlock.js
@@ -48,7 +48,7 @@ const EventLoopBlock = ({
 
   const [eventList, setEventList] = useState(null); // event data
 
-  const [gradeFilterId, setGradeFilterId] = useState(null); // data
+  const [gradeFilter, setGradeFilterId] = useState(null); // data
   const useEffectRef = useRef(null);
   const postLimitRef = useRef(0);
 
@@ -74,14 +74,17 @@ const EventLoopBlock = ({
     let eventList = Object.values(data);
     const grades = Object.values(state.source.event_grade);
 
-    let gradeFilterId = grades.filter(
-      (filter) => filter.name.toLowerCase() === grade_filter.toLowerCase()
-    )[0];
+    let gradeFilter = [];
+    if (grades)
+      grades.filter((filter) => {
+        // map grade_filter & if grade name matches grade_filter then return id
+        grade_filter.map((grade) => {
+          if (filter.name.toLowerCase() === grade.toLowerCase())
+            gradeFilter.push(filter.id);
+        });
+      });
 
-    if (gradeFilterId) gradeFilterId = gradeFilterId.id;
-
-    // if (post_limit) eventList = eventList.slice(0, Number(post_limit)); // apply limit on posts
-
+    console.log("gradeFilter", gradeFilter); // debug
     // sort events in order by date accenting from
     eventList = eventList.sort((a, b) => {
       // if !date_time return null;
@@ -91,7 +94,7 @@ const EventLoopBlock = ({
 
     // console.log("🚀 event list", eventList.length); // debug
     setEventList(eventList);
-    setGradeFilterId(gradeFilterId);
+    setGradeFilterId(gradeFilter);
 
     // link to anchor for event
     if (eventAnchor) {
@@ -141,8 +144,17 @@ const EventLoopBlock = ({
           } else {
             if (isArchive) return null;
           }
-          // ⬇️ show only events that grades matching filter grade
-          if (event_grade && !event_grade.includes(gradeFilterId)) return null;
+          // ⬇️ show only events that event_grade object have in common gradeFilter
+          if (gradeFilter.length > 0) {
+            if (!event_grade) return null;
+            let grade_match = false;
+            event_grade.forEach((grade) => {
+              gradeFilter.forEach((filter) => {
+                if (grade === filter) grade_match = true;
+              });
+            });
+            if (!grade_match) return null;
+          }
 
           if (searchFilter) {
             if (!title && !summary) return null;
