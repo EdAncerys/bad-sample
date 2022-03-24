1b09af7ac5b63cc2b3774ba32dcfdb4180c39769
Ed <lookatemail@gmail.com>
2022-03-24 19:19:24 +0000

    Add snags to contact form

--- packages/bad-theme/src/components/dashboard/updateHospitalDetails.js
+++ packages/bad-theme/src/components/dashboard/updateHospitalDetails.js
@@ -66,6 +66,7 @@ const UpdateHospitalDetails = ({ state, actions, libraries }) => {
         // contact_public_phone_number: "+1 (123) 456-7890",
         form_title: "Edit Professional Details",
         form_body: `Please use the form below to update us to changes to your professional details. To update your place of work, start typing to select from a list of options. If your place of work is not on the list, type Not Listed and use the message box at the bottom of the form instead`,
+        job_title: true,
         message: true,
         recipients: state.contactList.defaultContactList,
         isHospitalChange: true,
