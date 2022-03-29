d4ca3f1326c063b8752a4f764eb4065cc36c07e5
Dominik <dominik@skylarkcreative.co.uk>
2022-03-29 17:07:13 +0100

    Zoom on hover

--- packages/bad-theme/src/components/card/newsCarousel.js
+++ packages/bad-theme/src/components/card/newsCarousel.js
@@ -83,6 +83,7 @@ const NewsCarousel = ({ state, actions, libraries, newsCarousel }) => {
         style={{
           width: "100%",
           height: 250,
+          overflow: "hidden",
         }}
       >
         <Image
@@ -93,6 +94,7 @@ const NewsCarousel = ({ state, actions, libraries, newsCarousel }) => {
             height: "100%",
             objectFit: "cover",
           }}
+          className="card-image-animated"
         />
       </div>
     );
@@ -146,10 +148,13 @@ const NewsCarousel = ({ state, actions, libraries, newsCarousel }) => {
   };
 
   return (
-    <div className="position-relative">
+    <div className="position-relative heading-tile">
       <ServeImage />
       <ServePlaceholder />
-      <div className="position-absolute" style={{ top: `3em`, left: `2em` }}>
+      <div
+        className="position-absolute heading-tile"
+        style={{ top: `3em`, left: `2em` }}
+      >
         <div>
           <div
             className="flex-row"
