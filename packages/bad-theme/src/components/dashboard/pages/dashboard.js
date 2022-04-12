2e52e52a5ca6549542e3529b45702c511989c75e
Ed <lookatemail@gmail.com>
2022-04-12 08:48:02 +0000

    Coment out payment conponent

--- packages/bad-theme/src/components/dashboard/pages/dashboard.js
+++ packages/bad-theme/src/components/dashboard/pages/dashboard.js
@@ -428,7 +428,7 @@ const Dashboard = ({ state, actions, libraries }) => {
           </div>
         )}
         {/* <ServeApplicationStatus /> */}
-        {/* <ServePayments /> */}
+        <ServePayments />
       </div>
       <ServeEvents />
     </div>
