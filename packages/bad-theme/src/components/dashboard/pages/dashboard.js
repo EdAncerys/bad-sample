bfab74e21d1c8218a704bcf129c07e38fd42837b
Ed <lookatemail@gmail.com>
2022-04-14 08:48:21 +0000

    Add snags to dash event sorting

--- packages/bad-theme/src/components/dashboard/pages/dashboard.js
+++ packages/bad-theme/src/components/dashboard/pages/dashboard.js
@@ -51,8 +51,47 @@ const Dashboard = ({ state, actions, libraries }) => {
     let data = state.source.events;
     if (!data) await getEventsData({ state, actions });
     data = state.source.events;
+    if (data) data = Object.values(data);
+    // 📌 sort events by date newest first
+    data.sort((a, b) => {
+      let dateA = a.acf.date_time;
+      let dateB = b.acf.date_time;
+      if (dateA) dateA = dateA[0].date;
+      if (dateB) dateB = dateB[0].date;
+      // convert to date object
+      dateA = new Date(dateA);
+      dateB = new Date(dateB);
+
+      if (dateA > dateB) return -1;
+      if (dateA < dateB) return 1;
+      return 0;
+    });
+
+    // 📌 sort eventList by closest to today first (if date is set)
+    data.sort((a, b) => {
+      let dateA = a.acf.date_time;
+      let dateB = b.acf.date_time;
+      if (dateA) dateA = dateA[0].date;
+      if (dateB) dateB = dateB[0].date;
+
+      // convert to date object
+      dateA = new Date(dateA);
+      dateB = new Date(dateB);
+
+      // get today's date
+      let today = new Date();
+
+      // get date difference
+      let diffA = Math.abs(dateA - today);
+      let diffB = Math.abs(dateB - today);
+
+      if (diffA > diffB) return 1;
+      if (diffA < diffB) return -1;
+
+      return 0;
+    });
     // convert to object & return first 2 records
-    const events = Object.values(data).slice(0, 4);
+    const events = data.slice(0, 4);
     setEventList(events);
 
     return () => {
--- packages/bad-theme/src/components/dashboard/pages/dashboardEvents.js
+++ packages/bad-theme/src/components/dashboard/pages/dashboardEvents.js
@@ -39,7 +39,47 @@ const DashboardEvents = ({ state, actions, libraries, activeUser }) => {
       // throw exception if no events
       if (!data) throw new Error("Faild to fetch events data");
       data = state.source.events;
-      const events = Object.values(data).slice(0, 2);
+      if (data) data = Object.values(data);
+      // 📌 sort events by date newest first
+      data.sort((a, b) => {
+        let dateA = a.acf.date_time;
+        let dateB = b.acf.date_time;
+        if (dateA) dateA = dateA[0].date;
+        if (dateB) dateB = dateB[0].date;
+        // convert to date object
+        dateA = new Date(dateA);
+        dateB = new Date(dateB);
+
+        if (dateA > dateB) return -1;
+        if (dateA < dateB) return 1;
+        return 0;
+      });
+
+      // 📌 sort eventList by closest to today first (if date is set)
+      data.sort((a, b) => {
+        let dateA = a.acf.date_time;
+        let dateB = b.acf.date_time;
+        if (dateA) dateA = dateA[0].date;
+        if (dateB) dateB = dateB[0].date;
+
+        // convert to date object
+        dateA = new Date(dateA);
+        dateB = new Date(dateB);
+
+        // get today's date
+        let today = new Date();
+
+        // get date difference
+        let diffA = Math.abs(dateA - today);
+        let diffB = Math.abs(dateB - today);
+
+        if (diffA > diffB) return 1;
+        if (diffA < diffB) return -1;
+
+        return 0;
+      });
+
+      const events = data.slice(0, 2);
       setEventList(events);
 
       if (!isActiveUser) return null;
