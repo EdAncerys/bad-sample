493f7b15aeb611e11d04c0110659d5ca05d85e94
Dominik <dominik@skylarkcreative.co.uk>
2022-04-27 01:02:03 +0100

    Added a loading for search results

--- packages/bad-theme/src/components/searchDropDown.js
+++ packages/bad-theme/src/components/searchDropDown.js
@@ -4,7 +4,7 @@ import { colors } from "../config/imports";
 
 // CONTEXT ----------------------------------------------------------------
 import { postTypeHandler } from "../context";
-
+import Loading from "./loading";
 const SearchDropDown = ({
   state,
   actions,
@@ -20,11 +20,41 @@ const SearchDropDown = ({
   const Html2React = libraries.html2react.Component; // Get the component exposed by html2react.
 
   // filter value are one layer deep object with title & link { title: "", link: "" }
-  if (!dataLoading && !filter) return null;
-  if (!filter) return null;
 
+  if (!dataLoading && !filter) return null;
   const ctaHeight = 45;
   const BANNER_HEIGHT = state.theme.bannerHeight;
+
+  if (dataLoading && !filter)
+    return (
+      <div
+        className="input"
+        style={{
+          position: "absolute",
+          zIndex: 99,
+          left: 0,
+          right: 0,
+          marginTop: marginTop || 10,
+          border: `1px solid ${colors.silver}`,
+          backgroundColor: colors.white,
+        }}
+      >
+        <div className="flex">
+          <div
+            className="flex-col"
+            style={{
+              minHeight: ctaHeight,
+              maxHeight: BANNER_HEIGHT / 2,
+              borderRadius: 10,
+              padding: `0.5em 1em`,
+              overflow: "auto",
+            }}
+          >
+            <Loading />
+          </div>
+        </div>
+      </div>
+    );
 
   return (
     <div
