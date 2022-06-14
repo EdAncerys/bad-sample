825599f6c51fb61ffd3671915e3466615e58443c
Ed <lookatemail@gmail.com>
2022-06-14 12:56:29 +0000

    Refactor set auth cookie function

--- packages/bad-theme/src/helpers/setAuthenticationCookie.js
+++ packages/bad-theme/src/helpers/setAuthenticationCookie.js
@@ -3,8 +3,7 @@ import { fetchDataHandler } from "../context";
 export const setAuthenticationCookieAction = async ({ state, b2cTaken }) => {
   let path = state.auth.APP_HOST + "/users/b2c/auth?token=" + b2cTaken;
 
-  const response = await fetchDataHandler({ path, state });
-  const data = await response.json();
-
+  await fetchDataHandler({ path, state });
   // console.log("🍪 setAuthenticationCookieAction. 🍪", data); // debug
+  return null;
 };
