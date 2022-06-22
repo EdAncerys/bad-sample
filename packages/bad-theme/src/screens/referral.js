e48975d9766204804155fe60db42bd69959f4b6c
Ed <lookatemail@gmail.com>
2022-06-22 10:02:22 +0000

    Add typo fix to referrals

--- packages/bad-theme/src/screens/referral.js
+++ packages/bad-theme/src/screens/referral.js
@@ -295,7 +295,7 @@ const Referral = ({ state, actions, libraries }) => {
         <TitleBlock
           block={{
             text_align: "left",
-            title: "Please select desease severity:",
+            title: "Please select disease severity:",
           }}
           fontSize={20}
           margin="1em 0"
