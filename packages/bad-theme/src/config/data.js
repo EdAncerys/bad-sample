dde9b1323a73084e5459c67806489b9c78fc6759
Ed <lookatemail@gmail.com>
2022-06-28 10:54:04 +0000

    Add FAD to quicklinks

--- packages/bad-theme/src/config/data.js
+++ packages/bad-theme/src/config/data.js
@@ -112,6 +112,10 @@ export const MENU_DATA = [
     title: "Referrals",
     url: "/referrals/",
   },
+  {
+    title: "FAD",
+    url: "/find-a-dermatologist/",
+  },
 ];
 
 export const ALPHABET = [
