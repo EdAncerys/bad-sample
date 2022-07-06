43b91c3aa398e066a7fb5b7b2c53dbcc094a46d2
Ed <lookatemail@gmail.com>
2022-07-06 10:24:53 +0000

    Fix merge conflicts

--- packages/bad-theme/src/client.js
+++ packages/bad-theme/src/client.js
@@ -39,11 +39,7 @@ const BADTheme = {
       marginVertical: 40, // px units
       notification: false,
       perPageLimit: 50, // number of posts to fetch per page
-<<<<<<< HEAD
-      TICKET_NO: "B-19",
-=======
       TICKET_NO: "B-3m",
->>>>>>> b-3m
     },
     auth: {
       // 📌 app auth service secrets
