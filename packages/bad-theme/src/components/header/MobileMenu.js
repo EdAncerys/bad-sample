f55e73abf40c9c17d8aad5e4c17f9300da741dd8
Ed <lookatemail@gmail.com>
2022-07-29 14:11:39 +0000

    Rewrite mobile menu logic

--- packages/bad-theme/src/components/header/MobileMenu.js
+++ packages/bad-theme/src/components/header/MobileMenu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { connect } from "frontity";
 import { Nav } from "react-bootstrap";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
@@ -16,66 +16,75 @@ import { MENU_DATA } from "../../config/data";
 import { colors } from "../../config/imports";
 
 export default connect(({ libraries, state, actions, setMobileMenuActive }) => {
-  const [menuContent, setMenuContent] = React.useState();
-  const [wpMainMenu, setWpMainMenu] = React.useState([]);
-  // const [wpMoreMenu, setWpMoreMenu] = React.useState([]);
+  const [menuContent, setMenuContent] = useState(null);
+  const [wpMainMenu, setWpMainMenu] = useState(null);
   const { isActiveUser } = useAppState();
   const dispatch = useAppDispatch();
 
-  const setMenu = (children) => {
-    setMenuContent(children);
-  };
+  useEffect(() => {
+    // --------------------------------------------------------------------------------
+    // 📌  Add menu data to state forpm context
+    // --------------------------------------------------------------------------------
+    const data = state.theme.menu;
+    if (!data) return;
+    console.log("🐞 main_menu", data);
+
+    // set menu content as main menu data
+    setMenuContent(data);
+    setWpMainMenu(data); // main menu content
+  }, [state.theme.menu]);
 
   const onClickLinkHandler = async ({ title, url }) => {
+    // check if link is a Wiley link
     const isWileys =
-      title.includes("Journal") &&
-      !title.includes("SHD") &&
-      title !== "Journals";
+      title && title.includes("Journal") && !title.includes("SHD");
     let authLink = url;
+    console.log("🐞 authLink", authLink);
 
-    // HANDLERS ----------------------------------------------------
-    const handelLogin = () => {
-      setErrorAction({ dispatch, isError: null });
-      loginAction({ state });
-    };
+    // // HANDLERS ----------------------------------------------------
+    // const handelLogin = () => {
+    //   setErrorAction({ dispatch, isError: null });
+    //   loginAction({ state });
+    // };
 
-    const handleRedirect = () => {
-      setErrorAction({ dispatch, isError: null });
-      setGoToAction({ state, path: authLink, actions });
-    };
+    // const handleRedirect = () => {
+    //   setErrorAction({ dispatch, isError: null });
+    //   setGoToAction({ state, path: authLink, actions });
+    // };
 
-    // 📌 check if logged in user exists & user is BAD member to replace auth link
-    if (isWileys && isActiveUser) {
-      authLink = await getWileyAction({
-        state,
-        dispatch,
-        isActiveUser,
-        isFullAccess: true,
-        url,
-      });
-    }
+    // // 📌 check if logged in user exists & user is BAD member to replace auth link
+    // if (isWileys && isActiveUser) {
+    //   authLink = await getWileyAction({
+    //     state,
+    //     dispatch,
+    //     isActiveUser,
+    //     isFullAccess: true,
+    //     url,
+    //   });
+    // }
 
-    if (isWileys && !isActiveUser) {
-      // 📌 track notification error action
-      setErrorAction({
-        dispatch,
-        isError: {
-          message: `BAD members, make sure you are logged in to your BAD account to get free access to our journals. <br/> To continue to the publication without logging in, click 'Continue to Wiley website'`,
-          image: "Error",
-          action: [
-            {
-              label: `Continue to Wiley website`,
-              handler: handleRedirect,
-            },
-            { label: "Login", handler: handelLogin },
-          ],
-        },
-      });
-      return;
-    }
+    // if (isWileys && !isActiveUser) {
+    //   // 📌 track notification error action
+    //   setErrorAction({
+    //     dispatch,
+    //     isError: {
+    //       message: `BAD members, make sure you are logged in to your BAD account to get free access to our journals. <br/> To continue to the publication without logging in, click 'Continue to Wiley website'`,
+    //       image: "Error",
+    //       action: [
+    //         {
+    //           label: `Continue to Wiley website`,
+    //           handler: handleRedirect,
+    //         },
+    //         { label: "Login", handler: handelLogin },
+    //       ],
+    //     },
+    //   });
+    //   return;
+    // }
 
     setGoToAction({ state, path: authLink, actions });
   };
+
   const MenuNavItem = ({ item }) => {
     return (
       <div className="row" style={styles.navItem}>
@@ -89,22 +98,25 @@ export default connect(({ libraries, state, actions, setMobileMenuActive }) => {
     );
   };
 
-  const ServeMainMenu = () => {
-    return wpMainMenu.map((menu, key) => {
+  const ServeMenu = () => {
+    if (!menuContent) return null;
+
+    return menuContent.map((menu, key) => {
       return (
         <Nav.Link
           key={key}
           onClick={() => {
-            if (menu.child_items) {
-              setMenuContent({
-                main_title: menu.title,
-                main_slug: menu.slug,
-                main_url: menu.url,
-                children: menu.child_items,
-              });
+            const subMenu = menu.child_items;
+
+            if (subMenu) {
+              setMenuContent(subMenu);
+
+              console.log("🐞 subMenu", subMenu);
             } else {
               onClickLinkHandler({ title: menu.title, url: menu.url });
-              // toggleMobileMenu();
+
+              // disable menu on page naviagtion (toggleMobileMenu)
+              setMobileMenuActive(false);
             }
           }}
           style={styles.navMenuItem}
@@ -115,63 +127,22 @@ export default connect(({ libraries, state, actions, setMobileMenuActive }) => {
     });
   };
 
-  React.useEffect(() => {
-    // getting wp menu from state
-    const data = state.theme.menu;
-    if (!data) return;
+  const ServeAditionalLinks = () => {
+    // check if menu content is = wpMainMenu
+    if (menuContent !== wpMainMenu) {
+      return (
+        <Nav.Link onClick={() => setMenuContent(wpMainMenu)}>Go Back</Nav.Link>
+      );
+    }
 
-    setWpMainMenu(data); // main menu to display
-    // setWpMoreMenu(data); // more menu into dropdown
-  }, [state.theme.menu]);
-
-  if (menuContent) {
     return (
-      <div style={styles.container}>
-        <Nav.Link onClick={() => setMenuContent(null)}> Go Back</Nav.Link>
-        {menuContent.main_title ? (
-          <Nav.Link
-            style={{
-              ...styles.navItem,
-              ...styles.navMenuItem,
-              fontWeight: "bold",
-            }}
-            onClick={() => {
-              setGoToAction({ state, path: menuContent.main_url, actions });
-              // disable menu on page naviagtion (toggleMobileMenu)
-              setMobileMenuActive(false);
-            }}
-          >
-            <Parcer libraries={libraries} html={menuContent.main_title} />
-          </Nav.Link>
-        ) : null}
-
-        {menuContent.children.map((item, i) => {
-          return (
-            <Nav.Link
-              key={i}
-              style={styles.navMenuItem}
-              onClick={() => {
-                if (item.child_items) {
-                  setMenuContent({
-                    main_title: item.title,
-                    main_slug: item.slug,
-                    main_url: item.url,
-                    children: item.child_items,
-                  });
-                } else {
-                  onClickLinkHandler({ title: item.title, url: item.url });
-                  // disable menu on page naviagtion (toggleMobileMenu)
-                  setMobileMenuActive(false);
-                }
-              }}
-            >
-              <MenuNavItem item={item} />
-            </Nav.Link>
-          );
-        })}
-      </div>
+      <Nav.Link onClick={() => setMenuContent(MENU_DATA)}>
+        <MenuNavItem item={{ title: "Quick Links" }} />
+      </Nav.Link>
     );
-  }
+  };
+
+  if (!wpMainMenu) return null;
 
   return (
     <div
@@ -181,14 +152,8 @@ export default connect(({ libraries, state, actions, setMobileMenuActive }) => {
       data-aos-easing="ease-in-sine"
       data-aos-duration="300"
     >
-      <Nav.Link
-        onClick={() => {
-          setMenu({ children: MENU_DATA });
-        }}
-      >
-        <MenuNavItem item={{ title: "Quick Links" }} />
-      </Nav.Link>
-      <ServeMainMenu />
+      <ServeAditionalLinks />
+      <ServeMenu />
       <div style={{ height: "50px" }} />
     </div>
   );
